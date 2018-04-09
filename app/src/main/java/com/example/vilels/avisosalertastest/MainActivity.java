ebfbc85d30288d8bbc260885885c30ed92dcdde1
Vilels <34284284+Vilels@users.noreply.github.com>
2018-04-09 13:25:50 +0100

    Implementação do Realtime Database e do FCM
    Nova função que premite gravar o token do utilizador na bd de forma a poder ser acessivel via consola firebase para ser usado no envio de notificações como testes.

--- app/src/main/java/com/example/vilels/avisosalertastest/MainActivity.java
+++ app/src/main/java/com/example/vilels/avisosalertastest/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.vilels.avisosalertastest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends Activity {
+
+    public String tokenString;
+    protected Button buttonToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonToken = (Button) findViewById(R.id.buttonToken);
+        buttonToken.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                tokenString = FirebaseInstanceId.getInstance().getToken();
+                Toast.makeText(MainActivity.this, tokenString, Toast.LENGTH_SHORT).show();
+                sendRegistrationToServer(tokenString);
+            }
+        });
+    }
+
+    private void sendRegistrationToServer(String newToken){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("messsage");
+
+        myRef.setValue(newToken);
     }
 }
