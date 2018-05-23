e9413fe3a8870cf694ffb4b93674041f7dd9d16a
Vilels <34284284+Vilels@users.noreply.github.com>
2018-05-23 12:40:11 +0100

    Click on notifications opens an activity with the notification data

--- app/src/main/java/com/example/vilels/avisosalertastest/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/vilels/avisosalertastest/MyFirebaseMessagingService.java
@@ -5,8 +5,11 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -17,31 +20,46 @@ import static android.content.ContentValues.TAG;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+
         String click_action = remoteMessage.getNotification().getClickAction();
+        String title = "0";
+        String body = "0";
+        String key_1 = "0";
+        String key_2 = "0";
 
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        if (remoteMessage.getData().size()>0){
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            title = remoteMessage.getData().get("title");
+            body = remoteMessage.getData().get("body");
+            key_1 = remoteMessage.getData().get("key_1");
+            key_2 = remoteMessage.getData().get("key_2");
         }
 
-        super.onMessageReceived(remoteMessage);
-        Log.d("msg", "onMessageReceived: " + remoteMessage.getData().get("message"));
+        sendNotification(remoteMessage.getNotification().getBody(), remoteMessage.getNotification().getTitle(), title, body, key_1, key_2, click_action);
 
+
+    }
+
+    private void sendNotification(String messageBody, String messageTitle, String title, String body, String key_1, String key_2, String click_action){
         Intent intent = new Intent(click_action);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-        String channelId = "Default";
-        NotificationCompat.Builder builder = new  NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(remoteMessage.getNotification().getTitle())
-                .setContentText(remoteMessage.getNotification().getBody()).setAutoCancel(true).setContentIntent(pendingIntent);;
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(channelId, "Default channel", NotificationManager.IMPORTANCE_DEFAULT);
-            manager.createNotificationChannel(channel);
-        }
-        manager.notify(0, builder.build());
+        intent.putExtra("title", title);
+        intent.putExtra("body", body);
+        intent.putExtra("key_1", key_1);
+        intent.putExtra("key_2", key_2);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setSmallIcon(R.drawable.ic_home_black_24dp)
+                .setContentTitle(messageTitle)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notificationBuilder.build());
     }
 
 }
--- app/src/main/java/com/example/vilels/avisosalertastest/NotificationClickActivity.java
+++ app/src/main/java/com/example/vilels/avisosalertastest/NotificationClickActivity.java
@@ -1,7 +1,12 @@
 package com.example.vilels.avisosalertastest;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.widget.TextView;
 
 public class NotificationClickActivity extends Activity {
@@ -17,17 +22,21 @@ public class NotificationClickActivity extends Activity {
         data_key_1 = (TextView) findViewById(R.id.data_key_1);
         data_key_2 = (TextView) findViewById(R.id.data_key_2);
 
-        if (getIntent().getExtras()!=null){
-            for (String key : getIntent().getExtras().keySet()){
-                if (key.equals("title"))
-                    data_title.setText(getIntent().getExtras().getString(key));
-                else if (key.equals("body"))
-                    data_body.setText(getIntent().getExtras().getString(key));
-                else if (key.equals("key_1"))
-                    data_key_1.setText(getIntent().getExtras().getString(key));
-                else if (key.equals("key_2"))
-                    data_key_2.setText(getIntent().getExtras().getString(key));
-            }
-        }
+        Intent intent_o = getIntent();
+        String title = intent_o.getStringExtra("title");
+        String body = intent_o.getStringExtra("body");
+        String key_1 = intent_o.getStringExtra("key_1");
+        String key_2 = intent_o.getStringExtra("key_2");
+        data_title.setText(title);
+        data_body.setText(body);
+        data_key_1.setText(key_1);
+        data_key_2.setText(key_2);
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
